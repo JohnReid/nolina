d2418d6bd75482e05117656f64e1d4b1c7cda22e
JohnReid <johnbaronreid@netscape.net>
2020-09-10 18:59:51 +0100

    Add Rayleigh iteration method

--- nolina/inverse_iteration.py
+++ nolina/inverse_iteration.py
@@ -7,7 +7,7 @@ from nolina import random, normalise
 
 
 def inverse_iteration(A, lambda_, niter, y0=None, random_state=None):
-    lu, piv = lu_factor(A)
+    lu, piv = lu_factor(A - lambda_ * np.eye(A.shape[0]))
     return inverse_iteration_lu(lu, piv, lambda_, niter, y0=None, random_state=None)
 
 
@@ -19,4 +19,17 @@ def inverse_iteration_lu(lu, piv, lambda_, niter, y0=None, random_state=None):
         sigma = -1 if np.dot(y0, x0) < 0 else 1
         y0 *= sigma
         x0 = normalise(y0)
+    return lambda_ + 1 / la.norm(y0)
+
+
+def rayleigh_iteration(A, lambda_, niter, y0=None, random_state=None):
+    y0 = random.get_start_vector(d=A.shape[0], y0=y0, random_state=random_state)
+    x0 = normalise(y0)
+    for _ in range(niter):
+        lu, piv = lu_factor(A - lambda_ * np.eye(A.shape[0]))
+        y0 = lu_solve((lu, piv), x0)
+        sigma = -1 if np.dot(y0, x0) < 0 else 1
+        y0 *= sigma
+        x0 = normalise(y0)
+        lambda_ = x0.T @ A @ x0
     return lambda_ + 1 / la.norm(y0)
--- tests/test_inverse_iteration.py
+++ tests/test_inverse_iteration.py
@@ -5,12 +5,23 @@ import numpy.linalg as la
 from nolina import inverse_iteration, random
 
 
+def test_rayleigh_iteration(rng, seed):
+    A = random.random_spsd_matrix(d=10, random_state=rng)
+    w_eigh, v_eigh = la.eigh(A)
+
+    weighting = .9
+    lambda_ = weighting * w_eigh[-1] + (1 - weighting) * w_eigh[-2]
+    w_power = inverse_iteration.rayleigh_iteration(A, lambda_, niter=100, random_state=rng)
+
+    np.testing.assert_allclose(w_eigh[-1], w_power)
+
+
 def test_inverse_iteration(rng, seed):
     A = random.random_spsd_matrix(d=10, random_state=rng)
     w_eigh, v_eigh = la.eigh(A)
 
-    weighting = .99
+    weighting = .9
     lambda_ = weighting * w_eigh[-1] + (1 - weighting) * w_eigh[-2]
     w_power = inverse_iteration.inverse_iteration(A, lambda_=lambda_, niter=100, random_state=rng)
 
-    np.testing.assert_allclose(w_eigh[-1], w_power, rtol=0.01)
+    np.testing.assert_allclose(w_eigh[-1], w_power)
