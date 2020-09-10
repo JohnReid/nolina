5dec3a9d894cd188906919ec5f58842ea76bdda9
JohnReid <johnbaronreid@netscape.net>
2020-09-10 21:44:43 +0100

    Add conjugate gradient

--- nolina/conjugate_gradient.py
+++ nolina/conjugate_gradient.py
@@ -8,29 +8,71 @@ from typing import List
 
 @dataclass
 class GenericMinimiser:
+    """Algorithm 19."""
 
     A: List[float]
     b: List[float]
 
-    def __call__(self, x=None, eps=1e-8):
+    def __call__(self, x=None, eps=1e-12):
+        self.niter = 0
         if x is None:
             x = np.zeros_like(self.b)
         r = self.b - self.A @ x
+        p = self._initial_direction(x, r)
         while la.norm(r) > eps:
-            x, r = self._step(x, r)
+            x, p, r = self._step(x, p, r)
         return x
 
-    def _step(self, x, r):
-        p = self._choose_direction(x, r)
+    def _step(self, x, p, r):
         alpha = np.dot(r, p) / np.dot(self.A @ p, p)
         x = x + alpha * p
         r = self.b - self.A @ x
-        return x, r
+        p = self._choose_direction(x, p, r)
+        self.niter += 1
+        return x, p, r
 
-    def _choose_direction(self, x, r):
+    def _initial_direction(self, x, r):
+        return self._choose_direction(x, np.zeros_like(x), r)
+
+    def _choose_direction(self, x, p, r):
         raise NotImplementedError()
 
 
 class SteepestDescent(GenericMinimiser):
-    def _choose_direction(self, x, r):
+    """Algorithm 20."""
+
+    def _choose_direction(self, x, p, r):
         return r
+
+
+class ConjugateGradientPreliminary(GenericMinimiser):
+    """Algorithm 21."""
+
+    def _initial_direction(self, x, r):
+        return r
+
+    def _choose_direction(self, x, p, r):
+        beta = - np.dot(self.A @ p, r) / np.dot(self.A @ p, p)
+        return r + beta * p
+
+
+class ConjugateGradient(GenericMinimiser):
+    """Algorithm 22."""
+
+    def _step(self, x, p, r):
+        t = self.A @ p
+        old_r2 = np.dot(r, r)
+        alpha = old_r2 / np.dot(t, p)
+        x = x + alpha * p
+        r = r - alpha * t
+        beta = np.dot(r, r) / old_r2
+        p = r + beta * p
+        self.niter += 1
+        return x, p, r
+
+
+    def _initial_direction(self, x, r):
+        return r
+
+    def _choose_direction(self, x, p, r):
+        beta = - np.dot(A @ p, r) / np.dot(A @ p, p)
--- tests/test_conjugate_gradient.py
+++ tests/test_conjugate_gradient.py
@@ -1,10 +1,38 @@
+from codetiming import Timer
+import logging
+import pytest
 import numpy as np
 from nolina import random, conjugate_gradient as cg
 
+_logger = logging.getLogger(__name__)
+ds = [9, 21, 55]
 
-def test_steepest_descent(rng, seed):
-    d = 11
+
+@pytest.mark.parametrize("d", ds)
+def test_steepest_descent(d, rng, seed):
     minimiser = cg.SteepestDescent(A=random.random_spsd_matrix(d=d, random_state=rng),
                                    b=random.random_vector(d=d, random_state=rng))
-    x_star = minimiser()
+    with Timer(text='Steepest descent minimiser done in {:.4f} seconds', logger=_logger.info):
+        x_star = minimiser()
+    _logger.info('Steepest descent minimiser took %d iterations.', minimiser.niter)
+    np.testing.assert_allclose(minimiser.A @ x_star, minimiser.b)
+
+
+@pytest.mark.parametrize("d", ds)
+def test_conjugate_gradient_preliminary(d, rng, seed):
+    minimiser = cg.ConjugateGradientPreliminary(A=random.random_spsd_matrix(d=d, random_state=rng),
+                                                b=random.random_vector(d=d, random_state=rng))
+    with Timer(text='Conjugate gradient preliminary minimiser done in {:.4f} seconds', logger=_logger.info):
+        x_star = minimiser()
+    _logger.info('Conjugate gradient preliminary minimiser took %d iterations.', minimiser.niter)
+    np.testing.assert_allclose(minimiser.A @ x_star, minimiser.b)
+
+
+@pytest.mark.parametrize("d", ds)
+def test_conjugate_gradient(d, rng, seed):
+    minimiser = cg.ConjugateGradient(A=random.random_spsd_matrix(d=d, random_state=rng),
+                                     b=random.random_vector(d=d, random_state=rng))
+    with Timer(text='Conjugate gradient minimiser done in {:.4f} seconds', logger=_logger.info):
+        x_star = minimiser()
+    _logger.info('Conjugate gradient minimiser took %d iterations.', minimiser.niter)
     np.testing.assert_allclose(minimiser.A @ x_star, minimiser.b)
